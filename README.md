3addc98eb774e8d1acfb695cba8bcbd627682eba
Riccardo Bonafede <bonaff@live.it>
2015-11-04 13:18:35 +0100

    Refactoring, creato comando per uscire in modo pulito e fixato memory leak

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ gcc -o gol main.c -lncurses
 
 Usage
 =====
+$ ./gol
 
-Point and click for make new cell or to delete one.
-N in order to progress with new generation.(next function)
+Point and click with mouse for make new cell or to delete one.
+Press 'n' in order to progress with new generation.(next function)
--- main.c
+++ main.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ncurses.h>
-#include "main.h"
 
-#define ON 'x'
-#define OFF ' '
+#include "main.h"
 
 
 struct node **table;	//table[y][x]
+struct table_size memory_usage;
+
 
 	
 void init(void);
@@ -15,33 +12,49 @@ void getLife(void);
 void printTable(void);
 short int countNeightboor(short x, short y);
 void next(void);
+void quit(void);
+
 int main(int argc, char *argv[]){
-	
 	
 	initscr();      /* inizializza la libreria curses */ 
   	keypad(stdscr, TRUE);  /* abilita la mappatura della tastiera */ 
-    	nonl();         /* non convertire NL->CR/NL in output */ 
+    nonl();         /* non convertire NL->CR/NL in output */ 
    	//cbreak();       /* prende i caratteri in input uno alla volta, senza attendere il \n */ 
-    	noecho(); 
+    noecho(); 
 	curs_set(0);
 	mousemask(ALL_MOUSE_EVENTS, NULL);
 	init();
 	getLife();
-	
 }
 
 void init(void){
 	int i=0, a=0;
-	if((table= malloc(LINES*sizeof( struct node*)))==NULL){
-		fprintf(stderr, "ERROR: Out if memory\n");	
+
+
+	memory_usage.l=LINES;
+	memory_usage.c=COLS;
+
+	table= (struct node**) malloc((memory_usage.l) * sizeof( struct node*));
+	
+
+	if(table==NULL){
+		endwin();
+		fprintf(stderr, "Fatal: Out if memory\n");
+		exit(EXIT_FAILURE);
 	
 	};
 	
-	while(i<=LINES){
+	while(i<memory_usage.l){
 		a=0;
-		if((table[i]=(struct node*)malloc(COLS*sizeof(struct node)))==NULL){
-			fprintf(stderr, "ERROR: Out if memory\n");
+
+		table[i]= (struct node*) malloc((memory_usage.c) * sizeof(struct node));
+
+		if(table[i]==NULL){
+			endwin();
+			fprintf(stderr, "Fatal: Out if memory\n");
+			exit(EXIT_FAILURE);
 		}	
+
 		while(a<=COLS){
 			table[i][a].y=i;
 			table[i][a].x=a;
@@ -50,19 +63,20 @@ void init(void){
 		}
 		i++;
 	}
-	
 	refresh();
-	
 }
+
 void getLife(void){
 	int key;
 	MEVENT event;
 
-
 	while(1){
 		key=getch();
-		switch(key){
+
+		switch(key){ //andrebbe aggiunto il supporto a più funzioni(vedi auto play, pausa, salva, ecc)
+
 			case KEY_MOUSE:
+
 				if(getmouse(&event) == OK){
 					if(event.bstate & BUTTON1_CLICKED){
 						if(table[event.y][event.x].on==ON){
@@ -71,39 +85,51 @@ void getLife(void){
 						else if(table[event.y][event.x].on==OFF){
 							table[event.y][event.x].on=ON;
 						};
-					
-						
 					}
 				}
+
 				break;
+
 			case 'n':
 				
 				next();
 				break;
+
+			case 'q':
+
+				quit();
+				exit(EXIT_SUCCESS);
+				break;
+
+			default:
+				//donothing
+				break;
 		}
+
 		printTable();
 		key=0;
 		usleep(100);
 	};
-	
-	
 }
+
 void printTable(void){
 	int i=0, a=0;
 	
-	while(i<LINES){
+	while(i<memory_usage.l){
 		a=0;
-		while(a<COLS){
+
+		while(a<memory_usage.c){
+										//salva tutti i caratteri nel buffer, e fa un refresh alla schermata 
 			mvaddch(i,a,table[i][a].on);
 			table[i][a].last=table[i][a].on;
 			a++;
-			
 		}
 		i++;
 	}
 
 	refresh();
 }
+
 short int countNeightboor(short x, short y){
 	short int c=0,i=0;
 	struct node offset[8]={
@@ -120,16 +146,15 @@ short int countNeightboor(short x, short y){
 	###	uso le matrici soprastanti per determinare il numero dei figli...
 	#X#	Cosi' mi risparmio 8 if di cacca pieni di merda.
 	###	N.B. : risolto problema di x,y == 0
-		dio can del can, fuori di uno
+		//tolte un po' di parolacce
 	*/
 	
 	while(i<8){
-		
-		if((y+offset[i].y)<0 || (y+offset[i].y)>LINES){
+		if((y+offset[i].y)<0 || (y+offset[i].y)>memory_usage.l){ 
 				i++;
 				continue;
 		}
-		if((x+offset[i].x)<0 || (x+offset[i].x)>COLS){
+		if((x+offset[i].x)<0 || (x+offset[i].x)>memory_usage.c){
 				i++;
 				continue;	
 		}
@@ -138,29 +163,49 @@ short int countNeightboor(short x, short y){
 		}
 		i++;
 	}
-	if(c>8)
-		fprintf(stderr, "Somethings gone wrong in CountNeightboor");
+	if(c>8){
+		fprintf(stderr, "Fatal: Somethings gone wrong in CountNeightboor"); //Questo non dovrebbe accadere
+		endwin();
+		exit(EXIT_FAILURE);
+	}
 	return c;
 }
+
 void next(void){
 	short i=0, a=0,c=0;
 	
-	
-	while(i<LINES){
-		a=0;	
-		while(a<COLS){
-			
+	while(i<memory_usage.l){
+		a=0;
+
+		while(a<memory_usage.c){
 			c=countNeightboor(a, i);
 			
 			if(c==3 && table[i][a].on==OFF){	//NASCE
 				table[i][a].on=ON;
 			}
 			else if(!(c==2 || c==3) && table[i][a].on==ON){//CREPA
-				table[i][a].on=OFF;
-				
+				table[i][a].on=OFF;	
 			}
 			a++;
 		}
 		i++;
 	}	
+}
+
+void quit(){
+	int a=0;
+
+ 	endwin();
+ 	
+
+	while(a<memory_usage.l){
+		free(table[a]);
+		a++;
+	}
+
+	free((void **)table);
+
+	
+
+	return;
 }
